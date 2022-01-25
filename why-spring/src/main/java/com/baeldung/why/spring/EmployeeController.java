95571b743cc5fbf8059b861ad3bba1f7002a9152
edemuner <eduardood15@hotmail.com>
2022-01-24 22:15:22 -0300

    criado o controlador Rest

---
+++ why-spring/src/main/java/com/baeldung/why/spring/EmployeeController.java
@@ -0,0 +1,21 @@
+package com.baeldung.why.spring;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeRepository repository;
+
+    @GetMapping("/employees")
+    public List<Employee> getEmployees(){
+        return repository.findAll();
+    }
+
+    // Other CRUD endpoints handlers
+}
